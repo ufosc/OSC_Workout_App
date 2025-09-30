f4ae5eda93bde1b670db7aa6394260794bfeaaa0
ryder keeny <keenyryder@gmail.com>
2025-09-30 19:02:29 -0400

    initial setup for directories
    setup of directories for people to understand where things will go

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <p>workout app page</p>
-    </div>
+    <Router>
+      <div className="App">
+        <nav >
+          <NavLink 
+            to="/">
+            Home
+          </NavLink>
+          <NavLink 
+            to="/login" 
+
+          >
+            Login
+          </NavLink>
+          <NavLink 
+            to="/signup" 
+          >
+            Signup
+          </NavLink>
+        </nav>
+
+        <div>
+          <h1>Workout App</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
