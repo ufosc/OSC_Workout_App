6e1bb40330f34ba8e4107ea9c0648e807b5a521d
andi0316 <andriasubhit@gmail.com>
2025-10-06 23:22:27 -0400

    added skeleton

--- app/src/App.js
+++ app/src/App.js
@@ -8,32 +8,47 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav >
-          <NavLink 
-            to="/">
-            Home
-          </NavLink>
-          <NavLink 
-            to="/login" 
-
-          >
-            Login
-          </NavLink>
-          <NavLink 
-            to="/signup" 
-          >
-            Signup
-          </NavLink>
-        </nav>
 
         <div>
-          <h1>Workout App</h1>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
+
+        <div className='header-container'>
+          <div className='title-box'>
+            <h1 id='title'>Workout App</h1>
+            <input type='text' id='search-input' placeholder='Search for anything!'></input>
+          </div>
+
+          <div className='nav-box'>
+            <NavLink to="/">
+              Home
+            </NavLink>
+            <NavLink to="/login" >
+              Login
+            </NavLink>
+            <NavLink to="/signup" >
+              Signup
+            </NavLink>
+          </div>
+        </div>
+
+        <div className='content-container'>
+          <div className='content-photo'>
+            <h1>photo</h1>
+          </div>
+          <div className='content-box'>
+            <h1>content</h1>
+          </div>
+        </div>
+
+        <div className='more-content-container'>
+          <div> more content (slide deck?) </div>
+        </div>
+
       </div>
     </Router>
   );
