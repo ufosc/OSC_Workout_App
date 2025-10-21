78b57cee65ede101a174305e6a25814ae77d34da
bryan-bong <bryanbong38@gmail.com>
2025-10-21 02:06:08 -0400

    Added sequential shorts
    Shorts now autoplay and come in a sequence. Added a vertical carousel using Swiper.JS.
    
    To Do: Look into another API to autoplay AND unmute videos
    
    Youtube iframe disables Swiper scrolling, look into this. For now, only clicking between shorts.

--- src/Components/Brainrot.jsx
+++ src/Components/Brainrot.jsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/mousewheel';
+import 'swiper/css/pagination';
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-const SEARCH_QUERY = "brainrot shorts";
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;  // API Key from .env file
+const SEARCH_QUERY = "brainrot shorts";                 // Search query for youtube search. Can change to anything.
 
-// Backup Brainrot Shorts 
+// Backup Brainrot Shorts (Video IDs)
 const backups = [
     "cCT3xvpsdBQ",
     "Ayy8vG9EMFY",
     "CwawqpDLidQ"
 ]
 
-
+// Main Brainrot API Function
 function Brainrot() {
-    const[shortIDs, setShortIDs] = useState([]);
+    const[shortIDs, setShortIDs] = useState([]);    // Short video ID from Youtube for identification
 
     useEffect(() => {
         setShortIDs([]);
         const fetchShorts = async () => {
+            // Gets the video data from the search query using the API
             try {
                 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=7&type=video&videoDuration=short&q=${encodeURIComponent(
                     SEARCH_QUERY
@@ -29,7 +35,7 @@ function Brainrot() {
                 setShortIDs(brainrotIDs);
             }
             catch (error) {
-                // Occurs if the API is unable to find shorts.
+                // Occurs if the API is unable to find shorts. Plays backup videos.
                 console.error("Brainrot Acquisition Failed. Time to touch some grass!", error)
                 setShortIDs(backups);
             }
@@ -37,19 +43,31 @@ function Brainrot() {
         fetchShorts();
     }, []); 
 
+    // TBI Scrolling Shorts
     return(
         <div>
-            <h2>Brainrot</h2>
-            {shortIDs.map((id) => (
-                <iframe
-                    width="500"
-                    height="700"
-                    src={`https://www.youtube.com/embed/${id}`}
-                />
-            ))}
+            <h2>Take a brain break!</h2>
+            <Swiper
+                direction={'vertical'}
+                slidesPerView={1}
+                style={{ height: '700px', width: '500px'}}
+                pagination={{ clickable: true}}
+                modules={[Mousewheel, Pagination]}
+                >
+                {shortIDs.map((id) => (
+                    <SwiperSlide key={id}>
+                        <iframe
+                            width="500"
+                            height="700"
+                            src={`https://www.youtube.com/embed/${id}?autoplay=1&mute=1&loop=1&playlist=${id}`}
+                            allowFullScreen
+                        />
+                    </SwiperSlide>
+                ))}     
+            </Swiper>
         </div>
 
     );
-}
+};
 
 export default Brainrot;
