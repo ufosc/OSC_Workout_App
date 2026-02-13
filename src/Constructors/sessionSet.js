854870660a8f4ced514e800c2b7893e51f3d2b71
Matthew Starks <ukandit@hotmail.com>
2026-02-13 13:43:20 -0500

    Created set constructor

--- src/Constructors/sessionSet.js
+++ src/Constructors/sessionSet.js
@@ -1,29 +1,39 @@
+//weights
+//reps
+//time of set
 
-class SetObject {
-    constructor(weight=0,reps,time=0,type,activeTime=False){
+//https://drive.google.com/file/d/1gwcgkMZDAUTUM7klxQcF9IJGR9pfc_kk/view
+
+class Set {
+    constructor(weight = 0, reps = 0, time = 0, active = False) {
         this.weight = weight;
         this.reps = reps;
         this.time = time;
-        this.timeType=activeTime 
+        this.active = active;
     }
-    setWeight(newWeight){
-        this.weight=newWeight;
-    }
-    setReps(newReps){
-        this.reps=newReps;
-    }
-    setTime(newTime){
-        this.setTime=newTime;
-    }
-    getTime() {
-        return this.time;
-    }
+
     getWeight() {
         return this.weight;
     }
     getReps() {
         return this.reps;
     }
+    getTime() {
+        return this.time;
+    }
+    getActive() {
+        return this.active;
+    }
+    toggleActive() {
+        active = !active;
+    }
+    setTime(inputTime) {
+        this.time = inputTime;
+    }
+    setWeight(inputWeight) {
+        this.reps = inputWeight;
+    }
+    setReps(inputReps) {
+        this.reps = inputReps;
+    }
 }
-
-
